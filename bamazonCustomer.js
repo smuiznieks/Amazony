8a7964ff0cb25ec0ece882e479f26c5536a22cee
Selga Muiznieks <selga.muiznieks@gmail.com>
2018-01-21 13:25:12 -0500

    complete challenge 1 / customer view

---
+++ bamazonCustomer.js
@@ -0,0 +1,83 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+// Create connection information for the sql database
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '',
+    database: 'bamazon'
+});
+
+// Connect to the MySQL server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('\nWelcome to Amazon-y! You\'re connected as id ' + connection.threadId + '.\n');
+    // Once connected, print current inventory
+    printInventory();
+    // Then prompt user to enter information about their purchase
+    prompt();
+});
+
+function printInventory() {
+    connection.query('SELECT item_id, product_name, price FROM products', function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            var unitCost = res[i].price;
+            var finalPrice = unitCost.toFixed(2);
+            console.log('Item Id: ' + res[i].item_id + '  /  Product: ' + res[i].product_name + '  /  Price: $' + finalPrice);
+        }
+        console.log('\n');
+    });
+};
+
+function prompt() {
+    inquirer.prompt([
+        {
+            name: 'product',
+            type: 'input',
+            message: 'Enter the item ID of the product you would like to purchase:',
+            //validate: 
+        }, {
+            name: 'quantity',
+            type: 'input',
+            message: 'Enter the quantity you would like to order:',
+            // validate: 
+        }
+    ]).then(function(answer) {
+        connection.query('SELECT stock_quantity, price FROM products WHERE ?', 
+            { 
+                item_id: answer.product 
+            },
+            function(err, res) {
+                if (err) throw err;
+                if (res[0].stock_quantity < answer.quantity) {
+                    // If the order amount is greater than the stock quantity, the app prevents the order from going through
+                    console.log('\nSorry, we do not have enough of this item in stock.\n');
+                } else {
+                    // If the stock quantity is greater than or equal to the order amount, the order total is calculated and the database is updated to reflect the new inventory count
+                    var total = (res[0].price * answer.quantity);
+                    var final = total.toFixed(2);
+                    console.log('\nThank you for shopping with us!\nYour total is $' + final + '\n');
+                    var updatedInventory = parseInt(res[0].stock_quantity - answer.quantity);
+                    connection.query(
+                        'UPDATE products SET ? WHERE ?',
+                        [
+                            { 
+                                stock_quantity: updatedInventory
+                            },
+                            {
+                                item_id: answer.product
+                            }
+                        ],
+                        function(err, res) {
+                            if (err) throw err;
+                        }
+                    )
+                }
+                connection.end();
+            }
+        )
+    })
+};
---
+++ schema.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE products (
+    item_id INT(100) NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(20) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INT(100) NOT NULL,
+    PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Oreos', 'Pantry', 2.76, 70), 
+('Cheerios', 'Pantry', 2.96, 95), 
+('Book', 'Books', 19.95, 22), 
+('Audio book', 'Books', 9.99, 33), 
+('Camera', 'Electronics', 299.98, 3), 
+('Laptop', 'Electrionics', 599.00, 2), 
+('Vacuum', 'Electronics', 99.50, 10), 
+('Towels', 'Home', 9.90, 80), 
+('Duvet', 'Home', 45.20, 12), 
+('Lamp', 'Home', 25.44, 3);
+
+SELECT * FROM products;
+
+SET SQL_SAFE_UPDATES = 0;
